e51105c5e0b95f27dade1f8bed4a5f03cd582202
nurbol0tt <fairtail764@gmail.com>
2024-07-16 15:32:52 +0600

    write tasks

--- src/apps/user/tasks.py
+++ src/apps/user/tasks.py
@@ -1,14 +1,25 @@
 from django.core.mail import send_mail
+from django.urls import reverse
 
-from src.core.celery import app
+from core.celery import app
+
+from apps.user.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @app.task
-def send_message_email(data):
-    email_body = "Hi, it is your login and password from the account, don't show it to anyone\n" + \
-                 f"Login: {data.email}" + f" Password: {data.password}"
-    send_mail("Adding you to the program",
-              email_body,
-              "settings.EMAIL_HOST_USER",
-              [data.email]
-              )
+def send_message_email(email, current_site):
+    print(email)
+    user = User.objects.filter(email=email).first()
+    token = RefreshToken.for_user(user)
+    token['email'] = user.email  # Add email to the token payload
+    relativeLink = reverse('verify')
+    absurl = 'http://' + current_site + relativeLink + "?token=" + str(token.access_token)
+    email_body = 'Hi ' + user.username + \
+                 ' Use the link below to verify your email \n' + absurl
+    send_mail(
+        subject='Verify your email',
+        message=email_body,
+        from_email="settings.EMAIL_HOST_USER",
+        recipient_list=[user.email],
+    )
